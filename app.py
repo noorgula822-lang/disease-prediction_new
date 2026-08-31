a5c5ea009bfaac5668efa95e145d1bf854e692ac
ZAKIRKHAN <noorgula822@gmail.com>
2026-08-31 20:24:46 +0500

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-```python
+import os
 import pandas as pd
 import joblib
 from flask import Flask, request, render_template_string
@@ -386,9 +386,10 @@ def home():
 
 if __name__ == "__main__":
 
+    port = int(os.environ.get("PORT", 5000))
+
     app.run(
         host="0.0.0.0",
-        port=5000,
+        port=port,
         debug=False
     )
-```
